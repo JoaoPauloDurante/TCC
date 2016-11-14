37a8cc746c0c49defd48e170d1f26c0d5267c6d6
JoaoPauloDurante <joaopaulojphwa@gmail.com>
2016-11-14 17:44:13 -0200

    Revert "alterei o webservice exibe_cidade.php"
    This reverts commit 6fdb68b5f9f518e0b080a8cc7a060bca37358795.

--- Compare 11-11/WebServiceCompare/exibe_cidade.php
+++ Compare 11-11/WebServiceCompare/exibe_cidade.php
@@ -1,6 +1,9 @@
 <?php
 
-	ysqli_query($conexao, $sql);
+	$conexao = mysqli_connect("localhost","root","1234","comparev2");
+	$id = $_GET['id'];
+	$sql = "select * from cidades where estados_cod_estados = $id";
+	$result = mysqli_query($conexao, $sql);
 		
 	$registros = array();		
 	while ($linha = mysqli_fetch_array($result))
